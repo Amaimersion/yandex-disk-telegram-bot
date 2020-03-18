a4f25b720995d98d731d5e92dd47b0d5df3d5f55
Sergey Kuznetsov <amatsukine@gmail.com>
2020-03-18 22:42:40 +0300

    Add gunicorn config

---
+++ src/configs/gunicorn.py
@@ -0,0 +1,21 @@
+"""
+Configurations of the gunicorn server for specific environments.
+"""
+
+import os
+import multiprocessing
+
+
+is_development = (os.getenv("FLASK_ENV", "production") == "development")
+
+
+reload = is_development
+accesslog = "-"
+errorlog = "-"
+loglevel = ("debug" if is_development else "info")
+syslog = True
+# it is I/O bound app
+worker_class = "gevent"
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = multiprocessing.cpu_count() * 2 + 1
+worker_connections = 1000
