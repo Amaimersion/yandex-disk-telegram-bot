4907b47118842d25ee203e41ac2799d304f16824
Sergey Kuznetsov <amatsukine@gmail.com>
2020-03-19 16:10:44 +0300

    Add nginx server

--- scripts/wsgi/server.sh
+++ scripts/wsgi/server.sh
@@ -4,6 +4,8 @@ if [ "$1" = "flask" ]; then
   flask run --host=localhost --port 8000
 elif [ "$1" = "gunicorn" ]; then
   gunicorn --config ./src/configs/gunicorn.py wsgi:app
+elif [ "$1" = "nginx" ]; then
+  sudo nginx -p $PWD -c $PWD/src/configs/nginx.conf
 else
   echo "Invalid server name"
 fi
