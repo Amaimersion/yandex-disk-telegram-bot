a693b478cc925175c15479db7296ddab41324448
Sergey Kuznetsov <amatsukine@gmail.com>
2020-03-19 18:29:43 +0300

    Refactoring of server/run.sh

--- scripts/server/run.sh
+++ scripts/server/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = "flask" ]; then
-  ./scripts/server/run_flask.sh
+  source ./scripts/server/run_flask.sh
 elif [ "$1" = "gunicorn" ]; then
-  ./scripts/server/run_gunicorn.sh
+  source ./scripts/server/run_gunicorn.sh
 elif [ "$1" = "nginx" ]; then
-  ./scripts/server/run_nginx.sh
+  source ./scripts/server/run_nginx.sh
 else
   echo "Invalid server name"
 fi
