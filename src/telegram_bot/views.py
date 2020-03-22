aa0cca5247af6c61306df9b4f96df978eeb16ff5
Sergey Kuznetsov <amatsukine@gmail.com>
2020-03-22 20:55:51 +0300

    Raw handler for telegram bot

--- src/telegram_bot/views.py
+++ src/telegram_bot/views.py
@@ -1,5 +1,7 @@
 from flask import (
-    Blueprint
+    Blueprint,
+    request,
+    abort
 )
 
 
@@ -9,6 +11,59 @@ bp = Blueprint(
 )
 
 
-@bp.route("/")
+@bp.route("/", methods=["POST"], strict_slashes=False)
 def index():
+    data = request.get_json(
+        force=True,
+        silent=True,
+        cache=False
+    )
+
+    if (data is None):
+        abort(400)
+
+    message_is_valid = (
+        isinstance(
+            data.get("update_id"),
+            int
+        ) and
+        (
+            isinstance(
+                data.get("message"),
+                dict
+            ) or
+            isinstance(
+                data.get("channel_post"),
+                dict
+            )
+        )
+    )
+
+    if (not message_is_valid):
+        abort(400)
+
+    message = data["message"] or data["channel_post"]
+
+    message_2_is_valid = (
+        isinstance(
+            message.get("from"),
+            dict
+        ) and
+        (
+            isinstance(
+                message.get("entities"),
+                list
+            ) or
+            isinstance(
+                message.get("caption_entities"),
+                list
+            )
+        )
+    )
+
+    if (not message_2_is_valid):
+        abort(400)
+
+    print(message)
+
     return "Hello!"
