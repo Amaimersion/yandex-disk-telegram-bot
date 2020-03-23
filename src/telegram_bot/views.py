036e250cab10475902b1812e18ba7e74d1a5dbec
Sergey Kuznetsov <amatsukine@gmail.com>
2020-03-23 10:59:18 +0300

    Refactoring of index route of telegram bot

--- src/telegram_bot/views.py
+++ src/telegram_bot/views.py
@@ -13,6 +13,9 @@ bp = Blueprint(
 
 @bp.route("/", methods=["POST"], strict_slashes=False)
 def index():
+    """
+    Handles POST request from Telegram server.
+    """
     data = request.get_json(
         force=True,
         silent=True,
@@ -22,7 +25,26 @@ def index():
     if (data is None):
         abort(400)
 
-    message_is_valid = (
+    if (not data_is_valid(data)):
+        abort(400)
+
+    message = get_message(data)
+
+    if (not message_is_valid(message)):
+        abort(400)
+
+    entities = get_entities(message)
+    message_text = message["text"]
+    command = get_command(entities, message_text)
+
+    return "Hello!"
+
+
+def data_is_valid(data: dict) -> bool:
+    """
+    Checks submitted data for correctness.
+    """
+    return (
         isinstance(
             data.get("update_id"),
             int
@@ -39,15 +61,33 @@ def index():
         )
     )
 
-    if (not message_is_valid):
-        abort(400)
 
-    message = data["message"] or data["channel_post"]
+def get_message(data: dict) -> dict:
+    """
+    Extracts user message from submitted data.
+    """
+    return (
+        data.get("message") or
+        data.get("channel_post")
+    )
 
-    message_2_is_valid = (
+
+def message_is_valid(message: dict) -> bool:
+    """
+    Checks extracted message for correctness.
+    """
+    return (
+        isinstance(
+            message.get("message_id"),
+            int
+        ) and
         isinstance(
             message.get("from"),
             dict
+        ) and
+        isinstance(
+            message.get("text"),
+            str
         ) and
         (
             isinstance(
@@ -61,9 +101,63 @@ def index():
         )
     )
 
-    if (not message_2_is_valid):
-        abort(400)
 
-    print(message)
+def get_entities(message: dict) -> list:
+    """
+    Extracts entities from a message.
+    """
+    return (
+        message.get("entities") or
+        message.get("caption_entities")
+    )
 
-    return "Hello!"
+
+def entity_is_valid(entity: dict) -> bool:
+    """
+    Checks single entity for correctness.
+    """
+    return (
+        isinstance(
+            entity.get("type"),
+            str
+        ) and
+        isinstance(
+            entity.get("offset"),
+            int
+        ) and
+        isinstance(
+            entity.get("length"),
+            int
+        )
+    )
+
+
+def get_command(entities: list, message_text: str, default="/help") -> str:
+    """
+    Extracts bot command from entities.
+
+    Note: first command will be returned, even if the list
+    contains more than one command.
+
+    :param entities: Message entities.
+    :param message_text: Message text.
+    :param default: Default command which will be returned if
+    in message no any bot commands.
+    """
+    command = default
+
+    for entity in entities:
+        if (not entity_is_valid(entity)):
+            continue
+
+        if (not entity["type"] == "bot_command"):
+            continue
+
+        offset = entity["offset"]
+        length = entity["length"]
+        command = message_text[offset:offset + length]
+
+        # ignore next commands
+        break
+
+    return command
