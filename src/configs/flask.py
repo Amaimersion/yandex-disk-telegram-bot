07d5bdd3482522c779bcd1b2c07b55871eb2e41f
Sergey Kuznetsov <amatsukine@gmail.com>
2020-03-30 21:55:54 +0300

    Refactoring of flask config

--- src/configs/flask.py
+++ src/configs/flask.py
@@ -11,34 +11,37 @@ load_dotenv()
 
 
 class Config:
-    # Flask settings
+    # Flask
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
 
-    # SQLAlchemy settings
+    # SQLAlchemy
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Telegram API settings
+    # Telegram API
     # stop waiting for a Telegram response after a given number of seconds
     TELEGRAM_API_TIMEOUT = 5
 
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
+    TESTING = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SECRET_KEY = "development"
+    TESTING = False
+    SECRET_KEY = "q8bjscr0sLmAf50gXRFaIghoS7BvDd4Afxo2RjT3r3E="
     SQLALCHEMY_DATABASE_URI = "sqlite:///development.sqlite"
     SQLALCHEMY_ECHO = "debug"
 
 
 class TestingConfig(Config):
+    DEBUG = False
     TESTING = True
-    SECRET_KEY = "testing"
+    SECRET_KEY = "ReHdIY8zGRQUJRTgxo_zeKiv3MjIU-OYBD66GlW9ZKw="
     SQLALCHEMY_DATABASE_URI = "sqlite:///testing.sqlite"
 
 
