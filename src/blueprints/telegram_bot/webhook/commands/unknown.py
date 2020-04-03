4be68eb71eb0035304c1264f1ffc196bb22ae31d
Sergey Kuznetsov <amatsukine@gmail.com>
2020-04-03 20:48:33 +0300

    Fix using of wrong global variable in commands/unknown.py

--- src/blueprints/telegram_bot/webhook/commands/unknown.py
+++ src/blueprints/telegram_bot/webhook/commands/unknown.py
@@ -8,7 +8,7 @@ def handle():
     Handles unknown command.
     """
     telegram.send_message(
-        chat_id=g.incoming_message["id"],
+        chat_id=g.incoming_chat["id"],
         text=(
             "I can't understand this command. "
             "See command list or type /help"
