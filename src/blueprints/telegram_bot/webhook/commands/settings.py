309315f5922838f6775a67a05fafcb3dc8d1ad1a
Sergey Kuznetsov <amatsukine@gmail.com>
2020-04-03 19:53:46 +0300

    Add handler for /settings command

--- src/blueprints/telegram_bot/webhook/commands/settings.py
+++ src/blueprints/telegram_bot/webhook/commands/settings.py
@@ -1,14 +1,39 @@
 from flask import g
 
 from .....api import telegram
+from ..decorators import register_guest, get_db_data
 
 
+@register_guest
+@get_db_data
 def handle():
     """
     Handles `/settings` command.
     """
+    user = g.db_user
+    private_chat = g.private_chat
+
+    if (private_chat is None):
+        return
+
+    yd_access = False
+
+    if (
+        (user.yandex_disk_token) and
+        (user.yandex_disk_token.have_access_token())
+    ):
+        yd_access = True
+
+    text = (
+        "<b>Preferred language:</b> "
+        f"{user.language.name}"
+        "\n"
+        "<b>Yandex.Disk Access:</b> "
+        f"{'Granted' if yd_access else 'Not Granted'}"
+    )
+
     telegram.send_message(
-        chat_id=g.incoming_message["id"],
+        chat_id=private_chat.telegram_id,
         parse_mode="HTML",
-        text="/settings"
+        text=text
     )
