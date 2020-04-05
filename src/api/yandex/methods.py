e8b74e00468fd9c3ae5084da6cbb127f4b06b946
Sergey Kuznetsov <amatsukine@gmail.com>
2020-04-05 12:21:03 +0300

    Add request to Y.D. API for upload file with url

--- src/api/yandex/methods.py
+++ src/api/yandex/methods.py
@@ -1,6 +1,6 @@
 import requests
 
-from .request import make_oauth_request
+from .request import make_oauth_request, make_disk_request
 from .exceptions import YandexOauthAPIException
 
 
@@ -9,3 +9,10 @@ def get_access_token(**kwargs) -> dict:
     https://yandex.ru/dev/oauth/doc/dg/reference/auto-code-client-docpage/#auto-code-client__get-token
     """
     return make_oauth_request("token", kwargs)
+
+
+def upload_file_with_url(user_token, **kwargs) -> dict:
+    """
+    https://yandex.ru/dev/disk/api/reference/upload-ext-docpage/
+    """
+    return make_disk_request("resources/upload", kwargs, user_token)
